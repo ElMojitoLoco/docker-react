bd3b6e2c281f311a7ec37ce5d9a28f8f117da4b8
ElMojitoLoco <124294816+ElMojitoLoco@users.noreply.github.com>
2025-09-09 16:46:51 +0200

    trying to rebuild the image before deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi again there!
+          Hi again there! Beanstalk deployment test
         </p>
         <a
           className="App-link"
